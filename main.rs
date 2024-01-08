4f8a04f6ae058f2bd6f099cf624c25bd14bbd7c5
ferret <ferretbeansqwq@gmail.com>
2024-01-09 04:34:52 +0800

    update bcs i made it cool apparently

--- main.rs
+++ main.rs
@@ -1,13 +1,31 @@
-use std::io::{self, Write};
+use std::io::{self, stdin, stdout, Read, Write};
 use rand::Rng;
 use std::fs::OpenOptions;
 
-fn main() -> io::Result<()> {
+fn start() {
+    println!("Select whether you want to create or view passwords");
+    println!("1: create new password, 2 = view password");
+    let mut ans = String::new();
+    io::stdin()
+        .read_line(&mut ans)
+        .expect("failed to read");
+
+    let ans: u32 = ans.trim().parse().expect("please choose an option");
+
+    match ans {
+        1 => pw().expect("Error generating password"),
+        2 => viewer(),
+        _ => println!("Invalid option"),
+    }
+}
+
+fn pw() -> io::Result<()> {
     let char_arr = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ$*!@#";
     let mut num = String::new();
     print!("Enter the amount: ");
     io::stdout().flush()?;
     io::stdin().read_line(&mut num)?;
+
     let num: usize = num.trim().parse().expect("Invalid input");
 
     let mut password = String::new();
@@ -26,4 +44,21 @@ fn main() -> io::Result<()> {
     writeln!(writer, "{}", passwordtxt)?;
 
     Ok(())
+}
+
+fn viewer() {
+    let data = std::fs::read_to_string("./password.txt").expect("Error reading password file");
+    println!("{}", data);
+}
+
+fn pause() {
+    let mut stdout = stdout();
+    stdout.write(b"Press Enter to continue...").unwrap();
+    stdout.flush().unwrap();
+    stdin().read(&mut [0]).unwrap();
+}
+
+fn main() {
+    start();
+    pause();
 }
